77067fa9f125dfd467f5fb542ba98cd0e3a46f0a
andrewmckeighan <amm@iastate.edu>
2016-07-14 20:01:42 -0500

    second commit

---
+++ rumors/.metadata/.plugins/org.eclipse.core.resources/.history/97/a0d65297264a00161ea5fcdd03ae1781
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String line = "";
+		Scanner scan = new Scanner(System.in);
+		readInput(scan);
+	}
+
+	public static void readInput(Scanner scan){
+		while (scan.hasNextLine()) {
+			line = scan.nextLine();
+			while(line.equals("")){
+				line = scan.nextLine();
+			}
+		}
+		scan.close();
+	}
+}
--- rumors/Rumors/src/Main.java
+++ rumors/Rumors/src/Main.java
@@ -4,18 +4,33 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String line = "";
+		String read = "";
 		Scanner scan = new Scanner(System.in);
-		readInput(scan);
+		readInput(scan, read);
 	}
 
-	public static void readInput(Scanner scan){
+	public static void readInput(Scanner scan, String line){
 		while (scan.hasNextLine()) {
 			line = scan.nextLine();
 			while(line.equals("")){
 				line = scan.nextLine();
 			}
+			if(Integer.parseInt(line) == 0){
+				break;
+			}
+			storeMatrix(Integer.parseInt(line), scan);
 		}
 		scan.close();
+	}
+
+	public static void storeMatrix(int size, Scanner scan) {
+		int arr[][] = new int[size][size];
+		for(int i = 0; i < size; i++){
+			for(int j = 0; j<size; j++){
+				
+			}
+		}
+		
+		
 	}
 }
